a73e2df03a080d242e6c5b6ac957ff15131b4e64
Bakdaulet Berdikul <72352199+berd1kulov@users.noreply.github.com>
2022-04-09 18:37:56 +0600

    Initial Commit

---
+++ One-Lab2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  One-Lab2
+//
+//  Created by Bakdaulet Berdikul on 09.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
